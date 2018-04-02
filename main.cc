0a0afcae27002d5ea0278201159ee0e535c36f79
oopattern <oopattern@126.com>
2018-04-02 16:28:58 +0800

    add shm test function

--- main.cc
+++ main.cc
@@ -3,7 +3,8 @@
 #include "shmHash.h"
 
 
-void TestReadShm()
+// read shm once
+void TestOnceReadShm()
 {
     int uid = 2333;
     ValType user;
@@ -21,7 +22,36 @@ void TestReadShm()
     printf("shm read done\n");
 }
 
-void TestMutliWriteShm()
+// always change shm data
+void TestAlwaysWriteShm(void)
+{
+    int testUid = 2333;    
+    ValType user;
+    CShmHash shm;
+    
+    // first get user data
+    shm.AttachShm();
+    shm.ReadShm(testUid, (char*)&user, sizeof(user));
+    while (1)
+    {
+        // increase user money
+        user.money += 1;
+        if (user.money >= (30 * MILLION))
+        {
+            user.money = 0;
+            printf("pid:%d, uid:%d, too much money:%lld, clear it\n", ::getpid(), user.uid, user.money);
+        }
+
+        // just change shm data, not insert shm data 
+        if (SHM_OK != shm.WriteShm(user.uid, (char*)&user, sizeof(user), false))
+        {
+            printf("uid:%d, shm write error\n", user.uid);
+        }
+    }
+}
+
+// test how many data can be insert into shm
+void TestShmCapacity()
 {
     ValType user;
     CShmHash shm;
@@ -51,20 +81,24 @@ void TestMutliWriteShm()
         else 
         {
             // after read record, end the test
-            TestReadShm();
+            TestOnceReadShm();
             break;
         }
         usleep(5*1000);
     }
 }
 
-void TestBenchReadShm()
+// test shm read TPS, while read shm, need other threads process shm, simulate full load of CPU
+void TestReadShmTPS()
 {
     // confirm data record in shm
     const int QUERY_TIME = 20 * MILLION;
     ValType user;
     CShmHash shm;
     char timeBuf[64];
+
+    // other threads need to operate shm, simulate full load of CPU
+    // code not finish...
 
     printf("BenchMark QUERY_TIME=%s\n", ShowMagnitude(QUERY_TIME));
     GetCurrentTime(timeBuf, sizeof(timeBuf));
@@ -75,7 +109,7 @@ void TestBenchReadShm()
     int cnt = 0;
     while (cnt < QUERY_TIME)
     {
-        int uid = 2301 + cnt % 100;
+        int uid = 2333 + cnt % 1;
         if (SHM_OK != shm.ReadShm(uid, (char*)&user, sizeof(user)))
         {
             printf("shm read can not find uid=%d\n", uid);    
@@ -87,14 +121,15 @@ void TestBenchReadShm()
     GetCurrentTime(timeBuf, sizeof(timeBuf));
     printf("Query   end time: %s\n", timeBuf);
 
+    printf("uid:%d, money:%lld\n", user.uid, user.money);
     printf("shm read done\n");
 }
 
 int main(void)
 {
-    printf("hello world\n");
-    //TestMutliWriteShm();
-    TestBenchReadShm();
+    printf("hello world\n");    
+    //TestShmCapacity();
+    TestReadShmTPS();
     printf("shm test finish.\n");
     return 0;
 }
